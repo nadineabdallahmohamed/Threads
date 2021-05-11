23edc66d8c1fe0ecb8bc795a6fe46fbbc5d50f52
Nadine Abdallah <64757359+nadineabdallahmohamed@users.noreply.github.com>
2021-05-11 23:36:36 +0200

    Update and rename main.java to Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package my_threads;
+
+public class Main {
+	
+	public static void  main(String []args)
+	{
+		new TryThreads("Thread object 1").start();
+		new TryThreads("Thread object 2").start();
+		new Threads(new Simple("Runnable object ")).start();
+		
+	}
+
+}
